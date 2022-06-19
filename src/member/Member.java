b4b5b5c59d25504931b9239ccdf70104a7ea1811
Higiri <higiri.abukuma@gmail.com>
2022-06-20 01:09:46 +0900

    [fix] JavaDoc

--- src/member/Member.java
+++ src/member/Member.java
@@ -1,80 +1,95 @@
 package member;
 
+/**
+ * .
+ *
+ * @author Higiri
+ *
+ */
 public class Member {
+  /** 番号 */
   private int number;
+  /** 名前 */
   private String name;
+  /** なまえ */
   private String ruby;
+  /** 性別 */
   private String gender;
+  /** 誕生日 */
   private String birthday;
 
-  private static int NUM = 1;
-  private final int NUM_BASE = 220000;
+  /** 番号初期値 */
+  private static int COUNT = 1;
+  /** 番号ベース値 */
+  private final int BASE = 220000;
 
   /**
-   * コンストラクタ
-   * @param number
-   * @param name
-   * @param ruby
-   * @param gender
-   * @param birthday
+   * String配列でのコンストラクタ
+   *
+   * @param str - 配列 [0]名前 [1]ルビ [2]性別 [3]誕生日
    */
-  public Member(int number, String name, String ruby, String gender, String birthday) {
-    this.number = number;
-    this.name = name;
-    this.ruby = ruby;
-    this.gender = gender;
-    this.birthday = birthday;
-  }
-
-  public Member(String name, String ruby, String gender, String birthday) {
-    this.number = NUM_BASE + NUM++;
-    this.name = name;
-    this.ruby = ruby;
-    this.gender = gender;
-    this.birthday = birthday;
+  public Member(String[] str) throws NumberFormatException, ArrayIndexOutOfBoundsException {
+    this.number = BASE + COUNT++;
+    this.name = str[0];
+    this.ruby = str[1];
+    this.gender = str[2];
+    this.birthday = str[3];
   }
 
   /**
-   * @return number
+   * 番号取得するメソッド
+   *
+   * @return number - 番号
    */
   public int getNumber() {
     return number;
   }
 
   /**
-   * @return name
+   * 名前取得メソッド
+   *
+   * @return name - 名前
    */
   public String getName() {
     return name;
   }
 
   /**
-   * @return ruby
+   * なまえ取得メソッド
+   *
+   * @return ruby - なまえ
    */
   public String getRuby() {
     return ruby;
   }
 
   /**
-   * @return gender
+   * 性別取得メソッド
+   *
+   * @return gender - 性別
    */
   public String getGender() {
     return gender;
   }
 
   /**
-   * @return birthday
+   * 誕生日取得メソッド
+   *
+   * @return birthday - 誕生日
    */
   public String getBirthday() {
     return birthday;
   }
 
   /**
+   * 文字列化メソッド
    *
+   * @return 文字列
    */
   @Override
   public String toString() {
-    return this.number + ", " + this.name + ", " + this.gender + ", " + this.birthday;
+    return this.number + "," + this.name + ","
+        + this.ruby + "," + this.gender + "," + this.birthday;
   }
 
 }
