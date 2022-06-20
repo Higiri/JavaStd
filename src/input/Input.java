7d0215e8e39aa738de61f6bc13f6156a88e1bb16
Higiri <higiri.abukuma@gmail.com>
2022-06-21 00:59:43 +0900

    [add] Input: JavaDoc

--- src/input/Input.java
+++ src/input/Input.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 public class Input {
 
+  /**
+   * 数値入力
+   *
+   * @return 数値
+   */
+  @SuppressWarnings("resource")
   public static int inputNum() {
     Scanner stdin = new Scanner(System.in);
     try {
@@ -16,6 +22,12 @@ public class Input {
     return inputNum();
   }
 
+  /**
+   * 文字列入力
+   *
+   * @return 文字列
+   */
+  @SuppressWarnings("resource")
   public static String inputStr() {
     Scanner stdin = new Scanner(System.in);
 
